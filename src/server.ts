115f985d053ae0b21b7670eb1c2d141e1f10ce54
Keito654 <keito19981129@gmail.com>
2024-04-09 18:40:15 +0900

    追加

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("node.js");
+import express from "express";
+import userRoute from "./routes/users";
+
+const app = express();
+const PORT = 8000;
+
+// ミドルウェア
+app.use("/api/users", userRoute);
+
+app.listen(PORT, () => console.log("サーバーが起動しました。"));
